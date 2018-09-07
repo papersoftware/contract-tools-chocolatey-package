27c72b64f03590d2e28dad5894cc009a8020a706
Nathan Whetsell <nathan.whetsell@gmail.com>
2018-09-07 11:29:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# <img src="https://papersoftware.com/images/contract-tools-chocolatey-package-icon.png" width="48" height="48" /> [Contract Tools](https://chocolatey.org/packages/contract-tools)
+
+Contract Tools is a [Microsoft Word](https://products.office.com/en-us/word) [VSTO add‑in](https://docs.microsoft.com/en-us/visualstudio/vsto/create-vsto-add-ins-for-office-by-using-visual-studio) for drafting and analyzing contracts; visit https://papersoftware.com/contract-tools to learn more.
+
+After installing Contract Tools, you can [try it for free](https://contract-tools.helpscoutdocs.com/article/278-trying-contract-tools) for 7 days. After your trial, you can keep using Contract Tools by [entering the activation key](https://contract-tools.helpscoutdocs.com/article/143-activating-contract-tools) you receive when you buy a subscription. To buy a Contract Tools subscription, visit https://papersoftware.com.
+
+Contract Tools requires Word 2007 or later, but this package doesn’t include Word as a dependency. You should install Word before installing Contract Tools.
---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  softwareName   = 'Paper Software Contract Tools'
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'MSI'
+  url            = "https://papersoftware.s3.amazonaws.com/PaperSoftwareContractTools-$env:ChocolateyPackageVersion.msi"
+  checksum       = '9e05e44c9e5dd36ab252a652661364fc7563bccb84b183d94f91a8e3c1ea555f'
+  checksumType   = 'sha256'
+  silentArgs     = '/quiet /noRestart'
+  validExitCodes = @(0, 1641, 3010) # https://docs.microsoft.com/en-us/windows/desktop/Msi/error-codes
+}
+
+# On systems with Word 2007, Contract Tools requires .NET Framework 3.5 and must
+# be installed per-user.
+$keyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\winword.exe'
+$needsDotNet3_5 = $false
+if ((Test-Path $keyPath) -and (Get-ItemProperty $keyPath Path).Path -match '(\d+)\\$' -and $Matches[1] -eq 12) {
+  $needsDotNet3_5 = !(Test-Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5')
+  $packageArgs['silentArgs'] += ' ALLUSERS=""'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+if ($needsDotNet3_5) {
+  Write-Warning 'Contract Tools requires .NET Framework 3.5 on systems with Word 2007.'
+  Write-Warning 'To install .NET Framework 3.5, run:'
+  Write-Warning '  choco install dotnet3.5'
+}
